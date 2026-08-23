3c6303f3f0d8e48cc3594333a8b31a4050b59572
NhatHuy <huytrac2323@gmail.com>
2026-08-23 08:58:38 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 
 // Thêm import poolPromise từ db.js
 const { poolPromise } = require('./config/db');

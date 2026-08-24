714fd560b85cab3bd89f4cf0227101f37fcda6f3
NhatHuy <huytrac2323@gmail.com>
2026-08-24 21:19:43 +0700

    fix friendController.js

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,7 +1,7 @@
 // File: controllers/postController.js
+const pool = require('../config/db'); // <--- THÊM DÒNG NÀY VÀO ĐẦU FILE
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
-const pool = require('../config/db'); // <--- THÊM DÒNG NÀY VÀO ĐẦU FILE
 // Cấu hình Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,

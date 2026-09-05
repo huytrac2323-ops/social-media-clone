7b1bd2eab012e3a9a22ceca9475d81787a0c3799
NhatHuy <huytrac2323@gmail.com>
2026-09-05 20:26:58 +0700

    sửa postacard

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,5 +7,4 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout); // 👈 Thêm route đăng xuất
-
 module.exports = router;
